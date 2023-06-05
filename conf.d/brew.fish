c02d0bca9b8e371c111f250e280af18452a5a8e4
Pcrab <pcrab@pcrab.xyz>
2023-06-05 15:37:24 +0800

    feat: better homebrew

--- conf.d/brew.fish
+++ conf.d/brew.fish
@@ -1,4 +1,7 @@
 fish_add_path -p /opt/homebrew/bin
-fish_add_path -p /opt/homebrew/opt/curl/bin
+# fish_add_path -p /opt/homebrew/opt/curl/bin
 
 alias bu="brew update && brew upgrade && brew cu -a -y && brew cleanup"
+
+set -gx LIBRARY_PATH /opt/homebrew/lib $LIBRARY_PATH
+set -gx CPATH /opt/homebrew/include $CPATH
